01f1805d32998896cfc41646c38ec96a2cadeea6
aris.goico@gmail.com <aris.goico@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-03 21:09:40 +0000

    Primera implementación de hits, para probar fuerzas.

---
+++ trunk/proyecto/Assets/Scripts/MovementHit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementHit : MonoBehaviour {
+
+	public float speed;
+	public float force;
+	public GameObject leftArm;
+	public GameObject rightArm;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed));
+		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			//TODO Bloqueo
+		}
+		
+		if (Input.GetMouseButtonDown(0)) {
+			//TODO Golpe con la izquierda
+			leftArm.rigidbody.AddForce(Vector3.right * force, ForceMode.Impulse);
+		}
+		
+		if (Input.GetMouseButtonDown(1)) {
+			//TODO Golpe con la derecha
+			rightArm.rigidbody.AddForce(Vector3.right * force, ForceMode.Impulse);
+		}
+	}
+}
