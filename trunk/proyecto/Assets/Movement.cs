c45c0f2467d031afa4f4ba2224a04265bc304286
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-03 19:08:29 +0000

    initial setup

---
+++ trunk/proyecto/Assets/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal")*speed,0, Input.GetAxis("Vertical")*speed));
+	
+	}
+}
