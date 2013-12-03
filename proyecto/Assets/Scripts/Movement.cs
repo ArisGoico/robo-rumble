9b8373660aee3c64a7b6e60799d427cc37ce280c
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-03 21:31:43 +0000

    Movimiento 2

--- proyecto/Assets/Scripts/Movement.cs
+++ proyecto/Assets/Scripts/Movement.cs
@@ -7,35 +7,61 @@ public class Movement : MonoBehaviour {
 	public GUIText inputCapture;
 	public float speed;
 	public float dashSpeed = 1.0f;
+	public float dashDelay = 1.0f;
 	public float idleDrag = 5.0f;
 	public float hoverDrag = 0.75f;
 
+	public bool debug = false;
+
+	private bool hovering = false;
+	private bool dashing = false;
+
+
 	// Use this for initialization
 	void Start () {
-		//idea para el futuro: rigidbody.mass = suma de masa de los componentes.
+		//idea para el futuro: rigidbody.mass = suma de masa de los componentes, mass influye en los rigidbody addforce.
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		transform.rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"+player)*speed,0, Input.GetAxis("Vertical"+player)*speed));
-		if (Input.GetAxisRaw ("hover"+player) == 1) {
-			transform.rigidbody.drag = hoverDrag;
-			inputCapture.text = "hover";
-		} else {
-			transform.rigidbody.drag = idleDrag;
+		Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"+player)*speed,0, Input.GetAxis("Vertical"+player)*speed);
+		if (debug) {
+			Debug.DrawRay(transform.position, transform.forward, Color.red);
+			Debug.DrawRay(transform.position, moveDir, Color.green);
 		}
-		if (Input.GetAxisRaw ("dash"+player) == 1) {
+		transform.rigidbody.AddForce(moveDir);
+		hovering = Input.GetAxisRaw ("hover"+player) == 1 && !dashing;
+
+		if (hovering) {
+			transform.rigidbody.drag = hoverDrag;
+			inputCapture.text = "hovering";
+		} else { 
+			transform.rigidbody.drag = idleDrag;
+			inputCapture.text = "walking";
+		}
+
+		if (Input.GetAxisRaw ("dash"+player) == 1 && !dashing) {
+			dashing = true;
 			//genera movimiento casi instantaneo en la direccion pulsada o hacia adelante en caso de no indicar direccion)
-			Vector3 direction = (new Vector3(Input.GetAxis("Horizontal"+player),0, Input.GetAxis("Vertical"+player))).normalized;
-			if (direction.Equals (Vector3.zero)) {
+			transform.rigidbody.drag = idleDrag;
+			Vector3 dashDir = (new Vector3(Input.GetAxis("Horizontal"+player),0, Input.GetAxis("Vertical"+player))).normalized;
+			if (dashDir.Equals (Vector3.zero)) {
 				//TODO: revisar, puede que el forward del transform no sea el punto donde mira el robot. Seria torso.transform.forward;
-				direction = this.transform.forward;
+				dashDir = this.transform.forward;
 			}
 			//aplicar la fuerza
-			transform.rigidbody.AddForce(direction, ForceMode.Impulse);
+			if (debug) Debug.DrawRay(transform.position, dashDir*5.0f, Color.yellow);
+			transform.rigidbody.AddForce(dashDir*dashSpeed, ForceMode.Impulse);
 			inputCapture.text = "dash";
+			//dashing = false;
+			StartCoroutine(waitForDash(dashDelay));
 		} 
+	}
+
+	IEnumerator waitForDash (float time) {
+		yield return new WaitForSeconds(time);
+		dashing = false;
 	}
 }
