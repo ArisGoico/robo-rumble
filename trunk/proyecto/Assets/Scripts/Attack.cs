eb505927997bb0382d547f40e297b98410df53da
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-04 17:39:37 +0000

    Movimiento, ataque, escenario

---
+++ trunk/proyecto/Assets/Scripts/Attack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	public int player = 1;
+	public float force;
+	public GameObject leftArm;
+	public GameObject rightArm;
+	private ConfigurableJoint leftCJ;
+	private ConfigurableJoint rightCJ;
+
+	// Use this for initialization
+	void Start () {
+		leftCJ = leftArm.GetComponent<ConfigurableJoint>();
+		rightCJ = rightArm.GetComponent<ConfigurableJoint>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			//TODO Bloqueo
+		}
+		
+		if (Input.GetAxisRaw ("punch"+player) >0) {
+			//TODO Golpe con la izquierda
+			leftArm.rigidbody.AddForce(leftArm.transform.forward*force);
+		}
+		
+		if (Input.GetAxisRaw ("punch"+player) <0) {
+			//TODO Golpe con la derecha
+			rightArm.rigidbody.AddForce(transform.forward*force);
+		}
+	}
+}
