c62fb1494528e347f4d4987eaba3f0b25302f176
aris.goico@gmail.com <aris.goico@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-07 16:32:58 +0000

    Primera implementación de la logica del casco: energía y salud.

---
+++ proyecto/Assets/Scripts/HullLogic.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HullLogic : MonoBehaviour {
+
+	//Hull Integrity / Salud 
+	private int hullIntegrityCurrent		= 100;		//Current hull integrity / Current health
+	public int hullIntegrityMax				= 100;		//Max hull integrity / Max health
+	public float hullDensity				= 1f;		//Percentage of each hit damage that goes through (0 - 1)
+	public float hullPlating				= 0f;		//Min damage that goes through
+	
+	//Energy
+	public float energyCurrent				= 100f;		//Current energy
+	public float energyMax					= 100f;		//Max energy
+	public float energyRegenRate			= 10f;		//Energy regenerated per second
+	public float energyRegenDelay			= 0f;		//Number of seconds (/10) to start regenerating
+	
+	private float energyLastUsed			= 0f;		//Last time energy was used.
+	
+	
+	
+	void Start() {
+		hullIntegrityCurrent = hullIntegrityMax;
+		energyCurrent = energyMax;
+	}
+	
+	void Update() {
+		if (energyCurrent < energyMax && (energyLastUsed + ((float)energyRegenDelay / 10)) < Time.time) {
+			energyCurrent += (energyRegenRate * Time.deltaTime);
+		}
+		if (energyCurrent > energyMax) {
+			energyCurrent = energyMax;
+		}
+	}
+	
+	public bool consumeEnergy(float quantity) {
+		if (energyCurrent > quantity) {
+			energyCurrent -= quantity;
+			energyLastUsed = Time.time;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public bool damageHull(float quantity) {
+		float temp = hullIntegrityCurrent;
+		temp -= (quantity - hullPlating) * hullDensity;
+		if (temp < hullIntegrityCurrent) {
+			hullIntegrityCurrent = Mathf.RoundToInt(temp);
+			return true;
+		}
+		else 
+			return false;
+	}
+		
+}
