88efb978dd8c9016eb43bec48512d51881b32fee
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-09 18:10:55 +0000

    gastos de energia

--- trunk/proyecto/Assets/Scripts/Attack.cs
+++ trunk/proyecto/Assets/Scripts/Attack.cs
@@ -3,25 +3,36 @@ using System.Collections;
 
 public class Attack : MonoBehaviour {
 
-	public bool debug = false;
-	private int player = 1;
-	public float force;
-	public GameObject leftArm;
-	public GameObject rightArm;
-	private ConfigurableJoint leftCJ;
-	private ConfigurableJoint rightCJ;
-	private SoftJointLimit jointRelaxed;
-	private SoftJointLimit jointConstrained;
-	public bool punchingR = false;
-	public bool punchingL = false;
-	public bool blocking = false;
-	public float punchDelay = 0.5f;
-	public float relaxedLimit = 0.5f;
-	public float constrainedLimit = 0f;
+	public 	bool 				debug 				= false;
+	private int 				player 				= 1;
+	public 	float 				force;
+	public 	GameObject 			leftArm;
+	public 	GameObject 			rightArm;
+	public 	float 				punchConsume 		= 5f;
+	public 	float 				blockConsume 		= 0.3f;
+	public 	float 				blockTickConsume 	= 1.5f;
+
+	private HullLogic 			Energy;
+
+	//delays
+	public 	bool 				punchingR 			= false;
+	public 	bool 				punchingL 			= false;
+	public 	bool 				blocking 			= false;
+	public 	float 				punchDelay 			= 0.5f;
+	public 	float 				blockDelay 			= 0.5f;
+
+	//joints
+	private ConfigurableJoint 	leftCJ;
+	private ConfigurableJoint 	rightCJ;
+	private SoftJointLimit 		jointRelaxed;
+	private SoftJointLimit 		jointConstrained;
+	public 	float 				relaxedLimit 		= 0.5f;
+	public 	float 				constrainedLimit 	= 0f;
 
 	// Use this for initialization
 	void Start () {
 		player = transform.parent.GetComponent<Movement> ().player;
+		Energy = transform.GetComponent<HullLogic> ();
 		leftCJ = leftArm.GetComponent<ConfigurableJoint>();
 		rightCJ = rightArm.GetComponent<ConfigurableJoint>();	
 
@@ -48,9 +59,10 @@ public class Attack : MonoBehaviour {
 			//TODO Bloqueo
 		}
 		
-		if (Input.GetAxisRaw ("punch" + player) > 0 && !punchingL) {
+		if (Energy.energyCurrent - punchConsume > 0 && Input.GetAxisRaw ("punch" + player) > 0 && !punchingL) {
 			//TODO Golpe con la izquierda
 			punchingL = true;
+			Energy.consumeEnergy (punchConsume);
 			Debug.Log ("pega con la iqda");
 			leftArm.transform.Rotate (new Vector3(0,30,0));
 			leftCJ.linearLimit = jointRelaxed;  //con esto los brazos irian al cuerpo rapido si no esta pulsado, pero... delay y volver a linearLimit.limit= 0;
@@ -58,9 +70,10 @@ public class Attack : MonoBehaviour {
 			StartCoroutine(waitPunchL(punchDelay));
 		}
 		
-		if (Input.GetAxisRaw ("punch"+player) <0 && !punchingR) {
+		if (Energy.energyCurrent - punchConsume > 0 && Input.GetAxisRaw ("punch"+player) <0 && !punchingR) {
 			//TODO Golpe con la derecha
 			punchingR = true;
+			Energy.consumeEnergy (punchConsume);
 			rightArm.transform.Rotate (new Vector3(0,30,0));
 			Debug.Log ("pega con la dcha");
 			rightCJ.linearLimit = jointRelaxed;
--- trunk/proyecto/Assets/Scripts/HullLogic.cs
+++ trunk/proyecto/Assets/Scripts/HullLogic.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class HullLogic : MonoBehaviour {
 
 	//Hull Integrity / Salud 
+	public GameObject lifeBar;							//GUI Life graphic representation
 	public int hullIntegrityCurrent			= 100;		//Current hull integrity / Current health
 	public int hullIntegrityMax				= 100;		//Max hull integrity / Max health
 	public float hullDensity				= 1f;		//Percentage of each hit damage that goes through (0 - 1)
 	public float hullPlating				= 0f;		//Min damage that goes through
 	
 	//Energy
+	public GameObject energyBar;						//GUI Energy graphic representation
+	public GUIText energyLabel;							//GUI Energy text representation
 	public float energyCurrent				= 100f;		//Current energy
 	public float energyMax					= 100f;		//Max energy
 	public float energyRegenRate			= 10f;		//Energy regenerated per second
@@ -22,9 +25,11 @@ public class HullLogic : MonoBehaviour {
 	void Start() {
 		hullIntegrityCurrent = hullIntegrityMax;
 		energyCurrent = energyMax;
+		energyLabel.text = Mathf.FloorToInt(energyCurrent).ToString ();
 	}
 	
 	void Update() {
+		energyLabel.text = Mathf.FloorToInt(energyCurrent).ToString ();
 		if (energyCurrent < energyMax && (energyLastUsed + ((float)energyRegenDelay / 10)) < Time.time) {
 			energyCurrent += (energyRegenRate * Time.deltaTime);
 		}
