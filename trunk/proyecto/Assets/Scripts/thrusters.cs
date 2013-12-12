d210b8b39a99e430d31b2b10de93cdbe2bfc3da3
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-12 21:20:33 +0000

    Desierto del LOL

--- trunk/proyecto/Assets/Scripts/thrusters.cs
+++ trunk/proyecto/Assets/Scripts/thrusters.cs
@@ -21,14 +21,8 @@ public class thrusters : MonoBehaviour {
 		dashing = movingObject.GetComponent<Movement> ().getDashing ();
 		power = dashing ? 2.5f : 1f; 
 		moveDir = movingObject.GetComponent<Movement> ().getMoveDir ();
-		/*if (moveDir.magnitude < 4){
-			for (int i = 0; i< models.Length; i++) {
-				models[i].transform.localScale = Vector3.zero;
-			}
-		} else {*/
-			for (int i = 0; i< models.Length; i++) {
-				models [i].localScale = Vector3.Lerp (Vector3.zero, new Vector3(1f, power, 1f), Vector3.Dot (moveDir.normalized, models [i].transform.up) * moveDir.magnitude);
-			}
-		//}
+		for (int i = 0; i< models.Length; i++) {
+			models [i].localScale = Vector3.Lerp (Vector3.zero, new Vector3(1f, power, 1f), Vector3.Dot (moveDir.normalized, models [i].transform.up) * moveDir.magnitude);
+		}
 	}
 }
