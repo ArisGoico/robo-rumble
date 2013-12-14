2010b130a366c3ccdbb15c1756340a7850c1f7b9
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-14 00:31:35 +0000

    Blocking a medio hacer

--- trunk/proyecto/Assets/Scripts/Attack.cs
+++ trunk/proyecto/Assets/Scripts/Attack.cs
@@ -5,6 +5,7 @@ public class Attack : MonoBehaviour {
 
 	public 	bool 				debug 				= false;
 	private int 				player 				= 1;
+	private GameObject 			torso;
 	public 	float 				force;
 	public 	GameObject 			leftArm;
 	public 	GameObject 			rightArm;
@@ -19,28 +20,31 @@ public class Attack : MonoBehaviour {
 	public 	bool 				punchingL 			= false;
 	public 	bool 				blocking 			= false;
 	public 	float 				punchDelay 			= 0.5f;
-	public 	float 				blockDelay 			= 0.5f;
 
 	//joints
 	private ConfigurableJoint 	leftCJ;
 	private ConfigurableJoint 	rightCJ;
 	private SoftJointLimit 		jointRelaxed;
 	private SoftJointLimit 		jointConstrained;
+	private SoftJointLimit 		jointBlockingLimit;
 	public 	float 				relaxedLimit 		= 0.5f;
 	public 	float 				constrainedLimit 	= 0f;
 
 	// Use this for initialization
 	void Start () {
 		player = transform.parent.GetComponent<Movement> ().player;
+		torso = transform.parent.GetComponent<Movement> ().torso;
 		Energy = transform.GetComponent<HullLogic> ();
 		leftCJ = leftArm.GetComponent<ConfigurableJoint>();
 		rightCJ = rightArm.GetComponent<ConfigurableJoint>();	
 
 		jointRelaxed = new SoftJointLimit();
 		jointConstrained = new SoftJointLimit();
+		jointBlockingLimit = new SoftJointLimit ();
 
 		jointRelaxed.limit = relaxedLimit;
 		jointConstrained.limit = constrainedLimit;
+		jointBlockingLimit.limit = 90f;
 	}
 	
 	// Update is called once per frame
@@ -53,34 +57,65 @@ public class Attack : MonoBehaviour {
 			Debug.DrawLine (transform.position, transform.position + transform.forward, Color.blue);
 			Debug.DrawLine (leftArm.transform.position, transform.position + transform.forward);
 			Debug.DrawLine (rightArm.transform.position, transform.position + transform.forward);
+			Debug.DrawLine (leftArm.transform.position, (leftArm.transform.position - leftArm.transform.up* 0.5f + leftArm.transform.right* 0.4f ) , Color.red);
+			Debug.DrawLine (rightArm.transform.position, (rightArm.transform.position - rightArm.transform.up* 0.5f - rightArm.transform.right* 0.4f ) , Color.red);
+
 		}
 
-		if (Input.GetAxisRaw ("punchL" + player) > 0 && Input.GetAxisRaw ("punchL" + player) > 0 && !blocking) {
-			//TODO Bloqueo
-			Debug.Log ("Bloqueando" + Time.deltaTime);
+
+		//blocking
+		if (!punchingL && !punchingR && /*Input.GetAxisRaw ("punchL" + player) > 0 && Input.GetAxisRaw ("punchR" + player)*/Input.GetAxisRaw("lockMode"+player) > 0) {
+			if (debug) { Debug.Log ("Bloqueando" + Time.deltaTime); }
 			blocking = true;
-		} else if (!blocking) {
-			StartCoroutine(waitBlock(0.5f));
+
+			//relax joints
+			setJointforBlock(leftCJ, true);
+			setJointforBlock(rightCJ, true);
+
+			//rotate arm vertically
+			Quaternion rotation = Quaternion.LookRotation(transform.up) * Quaternion.AngleAxis(90, Vector3.forward);
+			leftArm.transform.rotation = Quaternion.Slerp(leftArm.transform.rotation, rotation, Time.deltaTime * 10f);
+
+			rightArm.transform.rotation = Quaternion.Slerp(leftArm.transform.rotation, rotation, Time.deltaTime * 10f);
+
+			//translate arm 
+			/*leftArm.transform.position = Vector3.Lerp (	leftArm.transform.position, 
+			                                           leftArm.transform.position + leftArm.transform.up* 0.5f - rightArm.transform.right* 0.4f,
+			                                           Time.deltaTime * 10f);
+			rightArm.transform.position = Vector3.Lerp ( rightArm.transform.position, 
+			                                            rightArm.transform.position + rightArm.transform.up* 0.5f - rightArm.transform.right* 0.4f, 
+			                                           Time.deltaTime * 10f);*/
+		} else if (blocking) {
+
+			//constrain joints
+			setJointforBlock(leftCJ, false);
+			setJointforBlock(rightCJ, false);
+			StartCoroutine(waitBlock(punchDelay));
 		}
 
+		//punching
 		if (Energy.energyCurrent - punchConsume > 0 && Input.GetAxisRaw ("punchL" + player) > 0 && !punchingL && !blocking) {
 			punchingL = true;
 			Energy.consumeEnergy (punchConsume);
 			if (debug)
 				Debug.Log ("pega con la iqda");
 			leftArm.transform.LookAt (transform.position + transform.forward);
-			leftCJ.linearLimit = jointRelaxed;  //con esto los brazos irian al cuerpo rapido si no esta pulsado, pero... delay y volver a linearLimit.limit= 0;
+			leftCJ.linearLimit = jointRelaxed;  
+			leftCJ.angularZMotion = ConfigurableJointMotion.Limited;
+			//leftCJ.angularYLimit = jointBlockingLimit;
 			leftArm.rigidbody.AddForce (lArmDirection * force, ForceMode.Impulse);
 			StartCoroutine(waitPunchL(punchDelay));
-		}
-		
-		if (Energy.energyCurrent - punchConsume > 0 && Input.GetAxisRaw ("punchR"+player) > 0 && !punchingR && !blocking) {
+		} 
+
+		if (Energy.energyCurrent - punchConsume > 0 && Input.GetAxisRaw ("punchR" + player) > 0 && !punchingR && !blocking) {
 			punchingR = true;
 			Energy.consumeEnergy (punchConsume);
 			rightArm.transform.LookAt (transform.position + transform.forward);
 			if (debug)
 				Debug.Log ("pega con la dcha");
 			rightCJ.linearLimit = jointRelaxed;
+			rightCJ.angularZMotion = ConfigurableJointMotion.Limited;
+			//rightCJ.angularYLimit = jointBlockingLimit;
 			rightArm.rigidbody.AddForce(rArmDirection*force, ForceMode.Impulse);
 			StartCoroutine(waitPunchR(punchDelay));
 		}
@@ -92,6 +127,8 @@ public class Attack : MonoBehaviour {
 			Debug.Log ("deja de pegarme con la derecha");
 		punchingR = false;
 		rightCJ.linearLimit = jointConstrained;
+		rightCJ.angularZMotion = ConfigurableJointMotion.Locked;
+
 	}
 
 	private IEnumerator waitPunchL(float time) {
@@ -100,6 +137,7 @@ public class Attack : MonoBehaviour {
 			Debug.Log ("deja de pegarme con la izqda");
 		punchingL = false;
 		leftCJ.linearLimit = jointConstrained;
+		leftCJ.angularZMotion = ConfigurableJointMotion.Locked;
 	}
 
 	private IEnumerator waitBlock(float time) {
@@ -107,6 +145,30 @@ public class Attack : MonoBehaviour {
 		if (debug)
 			Debug.Log ("deja de bloquear");
 		blocking = false;
+	}
+
+	private void setJointforBlock(ConfigurableJoint joint, bool block){
+		if (block) {
+			//relax joint
+			//joint.linearLimit = jointRelaxed;
+			joint.xMotion = ConfigurableJointMotion.Limited;
+			joint.yMotion = ConfigurableJointMotion.Locked;
+			joint.zMotion = ConfigurableJointMotion.Limited;
+			joint.angularXMotion = ConfigurableJointMotion.Limited;
+			joint.angularYMotion = ConfigurableJointMotion.Limited;
+			joint.highAngularXLimit = jointBlockingLimit;
+			joint.angularYLimit = jointBlockingLimit;
+		} else {
+			//constrain joint
+			joint.linearLimit = jointConstrained;
+			joint.xMotion = ConfigurableJointMotion.Locked;
+			joint.yMotion = ConfigurableJointMotion.Limited;
+			joint.xMotion = ConfigurableJointMotion.Locked;
+			joint.angularXMotion = ConfigurableJointMotion.Locked;
+			joint.angularYMotion = ConfigurableJointMotion.Locked;
+			joint.highAngularXLimit = jointConstrained;
+			joint.angularYLimit = jointConstrained;
+		}
 	}
 
 }
