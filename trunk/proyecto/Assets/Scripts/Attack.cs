da47e75e53b779b5c33dd883d9152aae6c61748d
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-14 17:59:03 +0000

    Bloqueo

--- trunk/proyecto/Assets/Scripts/Attack.cs
+++ trunk/proyecto/Assets/Scripts/Attack.cs
@@ -31,6 +31,7 @@ public class Attack : MonoBehaviour {
 	private SoftJointLimit 		jointRelaxed;
 	private SoftJointLimit 		jointConstrained;
 	private SoftJointLimit 		jointBlockingLimit;
+	private SoftJointLimit 		jointBlockingLimitInv;
 	public 	float 				relaxedLimit 		= 0.5f;
 	public 	float 				constrainedLimit 	= 0f;
 
@@ -51,6 +52,7 @@ public class Attack : MonoBehaviour {
 		jointRelaxed.limit = relaxedLimit;
 		jointConstrained.limit = constrainedLimit;
 		jointBlockingLimit.limit = 90f;
+		jointBlockingLimitInv.limit = -90f;
 	}
 	
 	// Update is called once per frame
@@ -59,35 +61,43 @@ public class Attack : MonoBehaviour {
 		Vector3 lArmDirection = transform.position + transform.forward - leftArm.transform.position;
 		Vector3 rArmDirection = transform.position + transform.forward - rightArm.transform.position;
 
-		if (debug) { 
-			Debug.DrawLine (transform.position, transform.position + transform.forward, Color.blue);
-			Debug.DrawLine (leftArm.transform.position, transform.position + transform.forward);
-			Debug.DrawLine (rightArm.transform.position, transform.position + transform.forward);
-			Debug.DrawLine (leftArm.transform.position, (leftArm.transform.position - leftArm.transform.up* 0.5f + leftArm.transform.right* 0.4f ) , Color.red);
-			Debug.DrawLine (rightArm.transform.position, (rightArm.transform.position - rightArm.transform.up* 0.5f - rightArm.transform.right* 0.4f ) , Color.red);
+		//blocking
+
+		if (debug){
+			Debug.DrawLine(transform.position, transform.position + transform.up, Color.green);
+			Debug.DrawLine(transform.position, transform.position + transform.forward, Color.blue);
+			Debug.DrawLine(transform.position, transform.position + transform.right, Color.red);
+
+			Debug.DrawLine(leftShoulder.transform.position, leftShoulder.transform.position + leftShoulder.transform.up*0.5f, Color.green);
+			Debug.DrawLine(leftShoulder.transform.position, leftShoulder.transform.position + leftShoulder.transform.forward*0.5f, Color.blue);
+			Debug.DrawLine(leftShoulder.transform.position, leftShoulder.transform.position + leftShoulder.transform.right*0.5f, Color.red);
+
+			//where to look with the arm
+			Debug.DrawLine(transform.position, leftShoulder.transform.position + transform.up*0.5f ,Color.cyan);
 
 		}
 
-
-		//blocking
-		if (((Input.GetAxisRaw ("punchL" + player) > 0 && Input.GetAxisRaw ("punchR" + player)) || Input.GetAxisRaw("lockMode"+player) > 0) && !punchingL && !punchingR ) {
+		if (((Input.GetAxisRaw ("punchL" + player) > 0 && Input.GetAxisRaw ("punchR" + player) > 0) || Input.GetAxisRaw("lockMode"+player) > 0) && !punchingL && !punchingR ) {
 			if (debug) { Debug.Log ("Bloqueando" + Time.deltaTime); }
 			blocking = true;
 
-			setJointforBlock(leftHJ, true);
-			setJointforBlock(rightHJ, true);
-			//TODO: no va brazo derecho T_T
-			Quaternion rotationL = Quaternion.AngleAxis (80, transform.forward) * Quaternion.AngleAxis (90, transform.right);
+			setJointforBlock(leftHJ, true, true);
+			setJointforBlock(rightHJ, true, false);
 
-			leftShoulder.transform.rotation = Quaternion.Slerp (leftShoulder.transform.rotation, rotationL, Time.deltaTime*5f);
-			//rightShoulder.transform.rotation = Quaternion.Slerp (rightShoulder.transform.rotation, rotationR, Time.deltaTime*10f);
+			//por Quaternions
+			//rightArm.rigidbody.AddForce(torso.transform.forward+torso.transform.right*2f);
+			//Quaternion rotationL = Quaternion.AngleAxis (70, transform.forward) * Quaternion.AngleAxis (5, transform.right); // funciona.
+			//Quaternion rotationR = Quaternion.AngleAxis (10, transform.position - rightArm.transform.localPosition + transform.right); // Quaternion.AngleAxis (90, rightArm.transform.forward);
 
+			//leftShoulder.transform.localRotation = Quaternion.Slerp (leftShoulder.transform.rotation, rotationL* leftShoulder.transform.forward, Time.deltaTime*5f);
+
+			//por lookAts
+			rightShoulder.transform.LookAt (rightShoulder.transform.position + transform.up, transform.right);
+			leftShoulder.transform.LookAt (leftShoulder.transform.position + transform.up, -transform.right);
 
 		} else if (blocking) {
 			StartCoroutine(waitBlock(punchDelay));
-		} else {
-			torso.GetComponent<HingeJoint>().useLimits = true;
-		}
+		} 
 
 		//punching
 		if (Energy.energyCurrent - punchConsume > 0 && Input.GetAxisRaw ("punchL" + player) > 0 && !punchingL && !blocking) {
@@ -138,23 +148,34 @@ public class Attack : MonoBehaviour {
 	}
 
 	private IEnumerator waitBlock(float time) {
-		yield return new WaitForSeconds(time);
+		yield return new WaitForSeconds(time*0.1f);
 		//constrain joints
-		setJointforBlock(leftHJ, false);
-		setJointforBlock(rightHJ, false);
+		setJointforBlock(leftHJ, false, false);
+		setJointforBlock(rightHJ, false, false);
 		if (debug)
 			Debug.Log ("deja de bloquear");
+		yield return new WaitForSeconds(time*0.8f);
 		blocking = false;
 	}
 
-	private void setJointforBlock(CharacterJoint joint, bool block){
+	private void setJointforBlock(CharacterJoint joint, bool block, bool left){
 		if (block) {
 			//relax joint
-			joint.highTwistLimit = jointBlockingLimit;
 			joint.swing2Limit = jointBlockingLimit;
+
+			if (left){
+				joint.highTwistLimit = jointBlockingLimit;
+				joint.lowTwistLimit = new SoftJointLimit();
+			}
+			else { 
+				joint.lowTwistLimit = jointBlockingLimitInv;
+				joint.highTwistLimit = new SoftJointLimit();
+			}
+
 		} else {
 			//constrain joint
 			joint.highTwistLimit = new SoftJointLimit();
+			joint.lowTwistLimit = new SoftJointLimit();
 			joint.swing2Limit = new SoftJointLimit();
 		}
 	}
