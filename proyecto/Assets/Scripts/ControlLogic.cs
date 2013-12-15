5a3b2e36a4c76cb0b79b7aac4d52872b9db29aa7
aris.goico@gmail.com <aris.goico@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-15 11:48:32 +0000

    Control de la escena sin funcionar correctamente...

--- proyecto/Assets/Scripts/ControlLogic.cs
+++ proyecto/Assets/Scripts/ControlLogic.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class ControlLogic : MonoBehaviour {
 
 	public GUIText textOpening;
-	public GUIText playerWins;
-	public GUIText playerWinsSubtitle;
+	public GUIText textEnding;
+	public GUIText textEndingSubtitle;
 
 	public Transform spawnPoint1;
 	public Transform spawnPoint2;
@@ -13,19 +13,77 @@ public class ControlLogic : MonoBehaviour {
 	public GameObject player1;
 	public GameObject player2;
 
+	private GameObject player1Spawn;
+	private GameObject player2Spawn;
+	private HullLogic player1Hull;
+	private HullLogic player2Hull;
+
 	private enum State {beginning, battle, win, lose};
 	private State state;
 
 	// Use this for initialization
 	void Start () {
-		Instantiate(player1, spawnPoint1.position, spawnPoint1.rotation);
-		Instantiate(player2, spawnPoint2.position, spawnPoint2.rotation);
-		state = State.beginning;
+		player1Spawn = Instantiate(player1, spawnPoint1.position, spawnPoint1.rotation) as GameObject;
+		player2Spawn = Instantiate(player2, spawnPoint2.position, spawnPoint2.rotation) as GameObject;
+		player1Hull = player1Spawn.GetComponentInChildren<HullLogic>();
+		player2Hull = player2Spawn.GetComponentInChildren<HullLogic>();
+		player1.SetActive(false);
+		player2.SetActive(false);
+		state = State.battle;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//TODO Falta controlar la vida, avanzar de estados, etc.
+		switch (state) {
+		case State.beginning:
+			if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) {
+				Destroy(player1Spawn);
+				Destroy(player2Spawn);
+//				player1Spawn = Instantiate(player1, spawnPoint1.position, spawnPoint1.rotation) as GameObject;
+//				player2Spawn = Instantiate(player2, spawnPoint2.position, spawnPoint2.rotation) as GameObject;
+//				player1Hull = player1Spawn.GetComponentInChildren<HullLogic>();
+//				player2Hull = player2Spawn.GetComponentInChildren<HullLogic>();
+				state = State.battle;
+			}
+			break;
+		case State.battle:
+			if (player1Hull.hullIntegrityCurrent < 0f) {
+				textEnding.text = "Player 2 wins!";
+				textEndingSubtitle.text = "Press enter to start again...";
+				state = State.win;
+			}
+			else if (player2Hull.hullIntegrityCurrent < 0f){
+				textEnding.text = "Player 1 wins!";
+				textEndingSubtitle.text = "Press enter to start again...";
+				state = State.win;
+			}
+
+			break;
+		case State.lose:
+			//TODO No se usa esto porque no hay red, pero se deja por si en el futuro la hay... ;)
+			break;
+		case State.win:
+			if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) {
+				Destroy(player1Spawn);
+				Destroy(player2Spawn);
+//				player1Spawn = Instantiate(player1, spawnPoint1.position, spawnPoint1.rotation) as GameObject;
+//				player2Spawn = Instantiate(player2, spawnPoint2.position, spawnPoint2.rotation) as GameObject;
+//				player1Hull = player1Spawn.GetComponentInChildren<HullLogic>();
+//				player2Hull = player2Spawn.GetComponentInChildren<HullLogic>();
+				state = State.battle;
+			}
+			break;
+		}
+
+		if (player1Spawn == null) {
+			player1Spawn = Instantiate(player1, spawnPoint1.position, spawnPoint1.rotation) as GameObject;
+			player1Hull = player1Spawn.GetComponentInChildren<HullLogic>();
+		}
+		if (player2Spawn == null) {
+			player2Spawn = Instantiate(player2, spawnPoint2.position, spawnPoint2.rotation) as GameObject;
+			player2Hull = player2Spawn.GetComponentInChildren<HullLogic>();
+		}
+
 	}
 
 	void OnGUI() {
@@ -37,28 +95,16 @@ public class ControlLogic : MonoBehaviour {
 			textOpening.enabled = false;
 		}
 
-		//Durante la batalla
-		if (state == State.battle) {
-			textOpening.enabled = true;
+		//Despues 
+		if (state == State.lose || state == State.win) {
+			textEnding.enabled = true;
+			textEndingSubtitle.enabled = true;
 		}
 		else {
-			textOpening.enabled = false;
+			textEnding.enabled = false;
+			textEndingSubtitle.enabled = false;
 		}
 
-		//Despues (perdido)
-		if (state == State.lose) {
-			textOpening.enabled = true;
-		}
-		else {
-			textOpening.enabled = false;
-		}
 
-		//Despues (ganado)
-		if (state == State.win) {
-			textOpening.enabled = true;
-		}
-		else {
-			textOpening.enabled = false;
-		}
 	}
 }
