dfed7030ec2d29b4adfe45ccc6e7adb1548ccf03
blackMafin@gmail.com <blackMafin@gmail.com@97ac105b-4e4a-d94a-1aff-d705c061d6b9>
2013-12-16 01:31:45 +0000

    efectos de golpe

---
+++ proyecto/Assets/autoDestroyParticleSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class autoDestroyParticleSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (particleSystem != null && particleSystem.particleCount == 0)
+			Destroy(gameObject,0.5f);
+	}
+}
